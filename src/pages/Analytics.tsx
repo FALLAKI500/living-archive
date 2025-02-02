d513a7ab6b1fc92bdcd2faa364bc0f989c1a18ba
FALLAKI500 <125271312+FALLAKI500@users.noreply.github.com>
2025-02-02 22:28:41 +0100

    ok

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,4 +1,5 @@
 import { Layout } from "@/components/Layout";
+import { Skeleton } from "@/components/ui/skeleton"
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "@/integrations/supabase/client";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
